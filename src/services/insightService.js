1f84eb162de739290415a8f2e281381f0279ff0d
VigneshPtcjose <vignesh.p@techjose.com>
2026-03-11 23:48:18 +0530

    updated the api

--- src/services/insightService.js
+++ src/services/insightService.js
@@ -1,6 +1,6 @@
 import axiosInstance from "../api/axiosInstance";
 
 export const getInsights = async () => {
-    const res = await axiosInstance.get('/insights');
+    const res = await axiosInstance.get('/api/insights');
     return res.data;
 };
--- src/services/transactionService.js
+++ src/services/transactionService.js
@@ -1,21 +1,21 @@
 import axiosInstance from "../api/axiosInstance";
 
 export const getTransactions = async (params) => {
-    const res = await axiosInstance.get('/transactions', { params });
+    const res = await axiosInstance.get('/api/transactions', { params });
     return res.data;
 };
 
 export const createTransaction = async (data) => {
-    const res = await axiosInstance.post('/transactions', data);
+    const res = await axiosInstance.post('/api/transactions', data);
     return res.data;
 };
 
 export const updateTransaction = async (id, data) => {
-    const res = await axiosInstance.put(`/transactions/${id}`, data);
+    const res = await axiosInstance.put(`/api/transactions/${id}`, data);
     return res.data;
 };
 
 export const deleteTransaction = async (id) => {
-    const res = await axiosInstance.delete(`/transactions/${id}`);
+    const res = await axiosInstance.delete(`/api/transactions/${id}`);
     return res.data;
 };
